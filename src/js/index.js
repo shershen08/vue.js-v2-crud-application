44ebd7a07940aabdb1b87766986e773695500d75
Mikhail Kuznetcov <michail.kuznetsov@gmail.com>
2016-11-14 15:57:05 +0100

    initial v1 commit

---
+++ src/js/index.js
@@ -0,0 +1,93 @@
+var products = [
+  {id: 1, name: 'Angular', description: 'Superheroic JavaScript MVW Framework.', price: 100},
+  {id: 2, name: 'Ember', description: 'A framework for creating ambitious web applications.', price: 100},
+  {id: 3, name: 'React', description: 'A JavaScript Library for building user interfaces.', price: 100}
+];
+
+function findProduct (productId) {
+  return products[findProductKey(productId)];
+};
+
+function findProductKey (productId) {
+  for (var key = 0; key < products.length; key++) {
+    if (products[key].id == productId) {
+      return key;
+    }
+  }
+};
+
+var List = Vue.extend({
+  template: '#product-list',
+  data: function () {
+    return {products: products, searchKey: ''};
+  }
+});
+
+var Product = Vue.extend({
+  template: '#product',
+  data: function () {
+    return {product: findProduct(this.$route.params.product_id)};
+  }
+});
+
+var ProductEdit = Vue.extend({
+  template: '#product-edit',
+  data: function () {
+    return {product: findProduct(this.$route.params.product_id)};
+  },
+  methods: {
+    updateProduct: function () {
+      var product = this.$get('product');
+      products[findProductKey(product.id)] = {
+        id: product.id,
+        name: product.name,
+        description: product.description,
+        price: product.price
+      };
+      router.go('/');
+    }
+  }
+});
+
+var ProductDelete = Vue.extend({
+  template: '#product-delete',
+  data: function () {
+    return {product: findProduct(this.$route.params.product_id)};
+  },
+  methods: {
+    deleteProduct: function () {
+      products.splice(findProductKey(this.$route.params.product_id), 1);
+      router.go('/');
+    }
+  }
+});
+
+var AddProduct = Vue.extend({
+  template: '#add-product',
+  data: function () {
+    return {product: {name: '', description: '', price: ''}
+    }
+  },
+  methods: {
+    createProduct: function() {
+      var product = this.$get('product');
+      products.push({
+        id: Math.random().toString().split('.')[1],
+        name: product.name,
+        description: product.description,
+        price: product.price
+      });
+      router.go('/');
+    }
+  }
+});
+
+var router = new VueRouter();
+router.map({
+  '/': {component: List},
+  '/product/:product_id': {component: Product, name: 'product'},
+  '/add-product': {component: AddProduct},
+  '/product/:product_id/edit': {component: ProductEdit, name: 'product-edit'},
+  '/product/:product_id/delete': {component: ProductDelete, name: 'product-delete'}
+})
+  .start(Vue.extend({}), '#app');
